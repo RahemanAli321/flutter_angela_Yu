dd046b60c975cf36bee61cd2ecc38936ffd2e567
DESKTOP-0206L39\Ali <rahemanali321@gmail.com>
2021-03-18 05:38:07 -0700

    flutter

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -9,86 +9,84 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        appBar: AppBar(
-          title: Text("Rich"),
-        ),
-        body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Column(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    margin: EdgeInsets.all(5),
-                    width: 100,
-                    height: 100,
-                    color: Colors.red,
-                    child: Text("container"),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    margin: EdgeInsets.all(5),
-                    width: 100,
-                    height: 100,
-                    color: Colors.green,
-                    child: Text("container"),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    margin: EdgeInsets.all(5),
-                    width: 100,
-                    height: 100,
-                    color: Colors.red,
-                    child: Text("container"),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    margin: EdgeInsets.all(5),
-                    width: 100,
-                    height: 100,
-                    color: Colors.green,
-                    child: Text("container"),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    margin: EdgeInsets.all(5),
-                    width: 100,
-                    height: 100,
-                    color: Colors.green,
-                    child: Text("container"),
-                  ),
-                ],
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage("images/pro.png"),
+                backgroundColor: Colors.transparent,
               ),
-              Row(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    width: 100,
-                    height: 100,
-                    color: Colors.blue,
-                    child: Text("container"),
+              Text(
+                "Raheman Ali",
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40,
+                  color: Colors.red,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "FLUTTER DEVELOPER",
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 10,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                width: 150,
+                height: 20,
+                child: Divider(
+                  color: Colors.teal.shade900,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.call,
+                      color: Colors.teal.shade900,
+                    ),
+                    title: Text(
+                      '+44 1234567890',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Sorce Sans Pro',
+                        fontSize: 20.0,
+                      ),
+                    ),
                   ),
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    width: 100,
-                    height: 100,
-                    color: Colors.black,
-                    child: Text("container"),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal.shade900,
+                    ),
+                    title: Text(
+                      'rahemanali321@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Sorce Sans Pro',
+                        fontSize: 20.0,
+                      ),
+                    ),
                   ),
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    width: 100,
-                    height: 100,
-                    color: Colors.blue,
-                    child: Text("container"),
-                  ),
-                ],
-              )
+                ),
+              ),
             ],
           ),
         ),
@@ -96,3 +94,23 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// Row(
+//                   children: [
+//                     Icon(
+//                       Icons.email,
+//                       color: Colors.teal.shade900,
+//                     ),
+//                     SizedBox(
+//                       width: 10.0,
+//                     ),
+//                     Text(
+//                       'rahemanali321@gmail.com',
+//                       style: TextStyle(
+//                         color: Colors.teal.shade900,
+//                         fontFamily: 'Sorce Sans Pro',
+//                         fontSize: 20.0,
+//                       ),
+//                     )
+//                   ],
+//                 ),
