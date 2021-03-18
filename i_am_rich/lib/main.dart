72abae6b584bae271748fe100451c40adf311efa
DESKTOP-0206L39\Ali <rahemanali321@gmail.com>
2021-03-17 23:25:20 -0700

    rich

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+        title: Center(child: Text("I AM Rich ")),
+      ),
+      body: ,
+      ),
+    );
+  }
+}
